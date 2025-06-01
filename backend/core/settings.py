96d9b4490ce265a50af48958f8c99485c85a6048
Mohamad Jamil <55766151+mohamad-jamil@users.noreply.github.com>
2025-06-01 13:56:36 +0100

    Create user model (#3)
    * Create accounts application
    
    * Define user model and make migrations
    
    * Add date_joined field to User model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -87,6 +88,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = "accounts.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
